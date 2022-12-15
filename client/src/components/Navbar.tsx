96ce05a3e452f116faef9488a606505181682577
Tron Nguyen <33529067+pnguyen46@users.noreply.github.com>
2022-12-15 16:14:50 -0500

    fix navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../auth/useUser";
 const Navbar = () => {
-  const redirectTo = useNavigate();  
+  const redirectTo = useNavigate();
+  const user = useUser();
   const logout = () => {
     localStorage.removeItem("token");
     redirectTo("/login");
@@ -9,6 +11,7 @@ const Navbar = () => {
   return (
     <nav className="border-b-2 border-black bg-white flex justify-end">
         <div>
+            <span className="mr-5">{user?.email}</span>
             <button className="border-2 border-black rounded-md p-1 mr-3 my-2 bg-black text-white" onClick={logout}>Logout</button>
         </div>
     </nav>
