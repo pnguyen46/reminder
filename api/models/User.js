edb7ca2fc75154d92b467427f25258e26bd9157c
Tron Nguyen <pnguye894@gmail.com>
2022-12-19 16:31:06 -0500

    remove api

--- api/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  info: Object,
-  isVerified: Boolean,
-});
-module.exports = mongoose.model("User", UserSchema);
